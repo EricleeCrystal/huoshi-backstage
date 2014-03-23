983e37c36e42c97d98d824fa85d505f0be4c5eba
EricleeCrystal <ericleecrystal1992@gmail.com>
2014-03-23 23:27:43 +0800

    add interceptor

---
+++ src/main/java/com/huoshi/im/interceptor/LoginInterceptor.java
@@ -0,0 +1,31 @@
+package com.huoshi.im.interceptor;
+
+import java.util.Map;
+import com.huoshi.im.util.ValueUtil.EmptyUtil;
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+
+@SuppressWarnings("serial")
+public class LoginInterceptor extends AbstractInterceptor {
+
+    @Override
+    public String intercept(ActionInvocation invocation) throws Exception {
+        // 取得请求相关的ActionContext实例
+        ActionContext ctx = invocation.getInvocationContext();
+        // 如果是login请求 不拦截
+        if (invocation.getInvocationContext().getName().equals("login")) {
+            return invocation.invoke();
+        }
+        Map<String, Object> session = ctx.getSession();
+        Integer opId = (Integer) session.get("opId");
+        String opName = (String) session.get("opName");
+        if (EmptyUtil.isNotEmpty(opId) && EmptyUtil.isNotEmpty(opName)) {
+            return invocation.invoke();
+        }
+        ctx.put("tip", "你还没有登录");
+        return Action.LOGIN;
+    }
+
+}
