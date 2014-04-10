edfef6d5515518e529252a443141fdcbccd9f891
EricleeCrystal <ericleecrystal1992@gmail.com>
2014-04-10 19:39:44 +0800

    add launcher

--- src/main/java/com/huoshi/im/action/LaunchAction.java
+++ src/main/java/com/huoshi/im/action/LaunchAction.java
@@ -1,10 +1,21 @@
 package com.huoshi.im.action;
 
+import lombok.Setter;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
 @SuppressWarnings("serial")
+@Service
+@Scope("prototype")
 public class LaunchAction extends BaseAction {
+
+    /** 0 全部 1 失效的 2 有效的 */
+    @Setter
+    private int expire;
 
     @Override
     public String execute() throws Exception {
+
         return super.execute();
     }
 }
--- src/main/webapp/launcher.jsp
+++ src/main/webapp/launcher.jsp
@@ -7,7 +7,7 @@ String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.
 <html>
   	<head>
     	<base href="<%=basePath%>">
-    	<title>图片管理</title>
+    	<title>启动图片管理</title>
 		<meta http-equiv="pragma" content="no-cache">
 		<meta http-equiv="cache-control" content="no-cache">
 		<meta http-equiv="expires" content="0">    
