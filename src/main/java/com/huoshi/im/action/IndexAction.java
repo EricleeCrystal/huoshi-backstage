66688d766263efb3f21d06a30956b7ebfc27d1be
EricleeCrystal <ericleecrystal1992@gmail.com>
2014-05-08 19:27:06 +0800

    index

---
+++ src/main/java/com/huoshi/im/action/IndexAction.java
@@ -0,0 +1,13 @@
+package com.huoshi.im.action;
+
+import org.springframework.stereotype.Service;
+
+@SuppressWarnings("serial")
+@Service
+public class IndexAction extends BaseAction {
+
+    @Override
+    public String execute() throws Exception {
+        return SUCCESS;
+    }
+}
